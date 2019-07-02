46457369d6686311b45b3d508e958cc93345ab20
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-07-01 21:10:11 -0400

    starting basic server

--- src/app.js
+++ src/app.js
@@ -1,34 +1,36 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+// const thingsRouter = require('./things/things-router')
+// const reviewsRouter = require('./reviews/reviews-router')
+const authRouter = require('./auth/auth-router')
+const usersRouter = require('./users/users-route')
 
-const app = express();
+const app = express()
 
-const morganOption = (NODE_ENV === 'production')
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test',
+}))
+app.use(cors())
+app.use(helmet())
 
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption));
-app.use(cors());
-app.use(helmet());
-
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+// app.use('/api/things', thingsRouter)
+// app.use('/api/reviews', reviewsRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/users', usersRouter)
 
 app.use(function errorHandler(error, req, res, next) {
-  let response;
+  let response
   if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error'} };
+    response = { error: 'Server error' }
   } else {
-    console.error(error);
-    response = { message: error.message, error};
+    console.error(error)
+    response = { error: error.message, object: error }
   }
-  res.status(500).json(response);
-});
+  res.status(500).json(response)
+})
 
-module.exports = app;
+module.exports = app
